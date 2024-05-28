dc1b7837f9aa7686eb4c8e1f073fb48a6305b0cb
EmilyLiu <emmalee1059@gmail.com>
2024-05-27 22:11:39 -0400

    split sql

--- flasktemplate/basic.py
+++ flasktemplate/basic.py
@@ -1,6 +1,5 @@
 # Set up your imports and your flask app.
 from flask import Flask, render_template, request
-import mysql.connector
 app = Flask(__name__)
 
 @app.route('/')
@@ -23,19 +22,3 @@ def report():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
-
-mydb = mysql.connector.connect(
-  host="172.31.26.182",
-  user="root",
-  password="Emily123$",
-  database="Northwind"
-)
-
-mycursor = mydb.cursor()
-
-mycursor.execute("SELECT * FROM Customers")
-
-myresult = mycursor.fetchall()
-
-for x in myresult:
-  print(x)
---
+++ flasktemplate/testsql.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="172.31.26.182",
+  user="root",
+  password="Emily123$",
+  database="Northwind"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM Customers")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
